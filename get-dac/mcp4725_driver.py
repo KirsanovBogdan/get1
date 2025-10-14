fa8cf2ef055428b4dbe511be7c2d0b0841397a7c
Kirs <bogdankirsanoff70@gmail.com>
2025-10-14 18:24:51 +0300

    fix

--- get-dac/mcp4725_driver.py
+++ get-dac/mcp4725_driver.py
@@ -5,38 +5,38 @@ class MCP4725:
         self.address = address
         self.wm = 0x00
         self.pds = 0x00
-
         self.verbose = verbose
         self.dynamic_range = dynamic_range
-
     def deinit(self):
         self.bus.close()
-    
-    def setnumber(self, number):
+    def set_number(self, number):
         if not isinstance(number, int):
-            print("DAC input can only take integer values")
-        if not (0 <= number <= 4096):
-            print("number out of bit depth of MCP7425 (12bit)")
-        
+            print("На вход ЦАП можно подавать только целые числа")
+        if not (0 <= number <= 4095):
+            print("Число выходит за разрядность MCP4752 (12 бит)")
         first_byte = self.wm | self.pds | number >> 8
         second_byte = number & 0xFF
         self.bus.write_byte_data(0x61, first_byte, second_byte)
 
         if self.verbose:
-            print(f"number: {number}, data to i2c: [0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n")
+            print(f"Число: {number}, отправленные по IC2 данные: [0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n")
+    def set_voltage(self,voltage):
+        if not(0 <= voltage <= self.dynamic_range):
+            print("Напряжение выходит за достпуные границы!")
+            return
 
-    def setvoltage(self, voltage):
-        self.setnumber(int(voltage/self.dynamic_range * 4096 - 1))
-    
+        digital_value = int((voltage / self.dynamic_range) * 4095)
+        digital_value = max(0, min(4095, digital_value))
+        self.set_number(digital_value)
 if __name__ == "__main__":
-    dac = MCP4725(5.16, 0x61, True)
     try:
+        dac = MCP4725(dynamic_range=5.11, verbose=True)
         while True:
             try:
-                voltage = float(input("input voltage i Volts: "))
-                dac.setvoltage(voltage)
+                voltage = float(input("Введите напряжение в Вольтах:"))
+                dac.set_voltage(voltage)
             except ValueError:
-                print("incorrect input, try again")
+                print("не то число")
     finally:
+        dac = MCP4725(dynamic_range=5.11, verbose=True)
         dac.deinit()
-
