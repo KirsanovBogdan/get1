ba14f4d4068fed341ecc66f83d43ac7c360e7bb0
Kirsanov Bogdan <bogdankirsanoff70@gmail.com>
2025-10-21 19:24:26 +0300

    plot

---
+++ get-adc/adc_plot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+
+def plot_voltage_vs_time(time, voltage, max_voltage):
+    plt.figure(figsize=(10,6))
+    plt.plot(time, voltage)
+    plt.show
---
+++ get-adc/r2r-sc.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+import RPi.GPIO as GPIO
+import time
+
+
+def plot_voltage_vs_time(time, voltage, max_voltage):
+    plt.figure(figsize=(10,6))
+    plt.plot(time, voltage)
+    plt.show()
+    
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
+class R2R_ADC:
+    def __init__(self, dynamic_range, compare_time = 0.01, verbose = False):
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+        self.compare_time = compare_time
+        
+        self.bits_gpio = [26, 20, 19, 16, 13, 12, 25, 11]
+        self.comp_gpio = 21
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.bits_gpio, GPIO.OUT, initial = 0)
+        GPIO.setup(self.comp_gpio, GPIO.IN)
+
+    def deinit(self):
+        GPIO.output(self.bits_gpio, 0)
+        GPIO.cleanup()
+
+    def number_to_dac(self, number):
+        for i in range(8):
+            GPIO.output(self.bits_gpio[i], dec2bin(number)[i])
+
+    def sequential_counting_adc(self):
+        for value in range (256):
+            self.number_to_dac(value)
+            time.sleep(self.compare_time)
+            compValue = GPIO.input(self.comp_gpio)
+            if compValue == 1:
+                return value
+        return 255
+
+    def get_sc_voltage(self):
+        digital_value = self.sequential_counting_adc()
+        voltage = (digital_value/255)*self.dynamic_range
+        return voltage
+
+
+
+if __name__ == "__main__":
+
+    voltage_values = []
+    time_values =[]
+    duration = 3.0
+    try:
+        adc = R2R_ADC(3.3)
+        start_time = time.time()
+
+        while time.time() - start_time < duration:
+            voltage = adc.get_sc_voltage()
+            print(f"voltage: {voltage:.3f} V")
+            print(f"time: {time.time() - start_time:.3f} s")
+            time_values.append(time.time()-start_time)
+            voltage_values.append(voltage)
+
+        plot_voltage_vs_time(time_values, voltage_values, adc.dynamic_range)
+    except KeyboardInterrupt:
+        print("Stopped by the keyboard\n")
+    finally:
+        adc.deinit()
+
