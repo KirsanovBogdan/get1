96689f296baac5e11d11dd5ad3d1330e838c2279
Kirsanov Bogdan <bogdankirsanoff70@gmail.com>
2025-10-21 18:46:54 +0300

    1sr adc done

--- get-adc/r2r_adc.py
+++ get-adc/r2r_adc.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
 class R2R_ADC:
     def __init__(self, dynamic_range, compare_time = 0.01, verbose = False):
         self.dynamic_range = dynamic_range
@@ -13,3 +16,41 @@ class R2R_ADC:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.bits_gpio, GPIO.OUT, initial = 0)
         GPIO.setup(self.comp_gpio, GPIO.IN)
+
+    def deinit(self):
+        GPIO.output(self.bits_gpio, 0)
+        GPIO.cleanup()
+
+    def number_to_dac(self, number):
+        for i in range(8):
+            GPIO.output(self.bits_gpio[i], dec2bin(number)[i])
+
+    def sequential_counting_adc(self):
+        for value in range (256):
+            self.number_to_dac(value)
+            time.sleep(self.compare_time)
+            compValue = GPIO.input(self.comp_gpio)
+            if compValue == 1:
+                return value
+        return 255
+
+    def get_sc_voltage(self):
+        digital_value = self.sequential_counting_adc()
+        voltage = (digital_value/255)*self.dynamic_range
+        return voltage
+
+
+
+if __name__ == "__main__":
+    try:
+        adc = R2R_ADC(3.3)
+
+        while True:
+            voltage = adc.get_sc_voltage()
+            print(f"voltage: {voltage:.3f} V")
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("Stopped by the keyboard\n")
+    finally:
+        adc.deinit()
+
